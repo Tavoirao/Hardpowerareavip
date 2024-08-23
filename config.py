68457aa2252424e2918aec99927ec328d8a48d03
Tavoirao <gugams9191@gmail.com>
2024-08-23 20:02:35 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'Tavoiro123!'
+    MYSQL_HOST = 'bancohardpw2.mysql.dbaas.com.br'
+    MYSQL_USER = 'bancohardpw2'
+    MYSQL_PASSWORD = 'Q@or2tigQ@ZM'
+    MYSQL_DB = 'bancohardpw2'
+    MYSQL_PORT = 3306
